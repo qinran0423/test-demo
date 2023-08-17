3c949cdf29598b58b5141b85746f7d3e8b6755f3
qinran0423 <1156107187@qq.com>
2023-08-17 22:45:59 +0800

    fix: add test func

--- mini-test-runner-demo/first.spec.js
+++ mini-test-runner-demo/first.spec.js
@@ -1,6 +1,12 @@
-import { expect, test, it, run } from './index.js';
+import { describe, afterEach, afterAll, beforeAll, beforeEach, expect, test, it, run } from './index.js';
 
+beforeAll(() => {
+	console.log('before all')
+})
 
+beforeEach(() => {
+	console.log('before each')
+})
 test('test1', () => {
 	console.log('this is test1')
 	expect(2).toBe(2)
@@ -8,6 +14,22 @@ test('test1', () => {
 
 it('it', () => {
 	console.log('this is it')
+	expect(2).toBe(3)
 })
+
+afterEach(() => {
+	console.log('after each')
+})
+
+afterAll(() => {
+	console.log('after all')
+})
+
+describe('sub', () => {
+	test("sub:only test case", () => {
+		console.log('sub:only test case')
+	})
+})
+
 
 run()
--- mini-test-runner-demo/index.js
+++ mini-test-runner-demo/index.js
@@ -1,16 +1,41 @@
-
-
-
 const tests = []
+const onlys = []
+const beforeAlls = []
+const beforeEachs = []
+const afterAlls = []
+const afterEachs = []
 export function test(name, callback) {
 	tests.push({ name, callback })
+}
+
+export function beforeAll(callback) {
+	beforeAlls.push(callback)
+}
+
+export function beforeEach(callback) {
+	beforeEachs.push(callback)
+}
+
+export function afterAll(callback) {
+	afterAlls.push(callback)
+}
+
+export function afterEach(callback) {
+	afterEachs.push(callback)
+}
+
+export function describe(name, callback) {
+	callback()
+
+}
+test.only = (name, callback) => {
+	onlys.push({ name, callback })
 }
 
 export function expect(actual) {
 	return {
 		toBe(expected) {
 			if (expected === actual) {
-				console.log('ok')
 			} else {
 				throw Error(`fail actual: ${actual} expected: ${expected}`)
 			}
@@ -20,9 +45,28 @@ export function expect(actual) {
 
 export const it = test
 
-
 export function run() {
-	for (const test of tests) {
-		test.callback()
+	for (const beforeAllCallback of beforeAlls) {
+		beforeAllCallback()
+	}
+	const suit = onlys.length > 0 ? onlys : tests
+
+	for (const test of suit) {
+		for (const beforeEachCallback of beforeEachs) {
+			beforeEachCallback()
+		}
+		try {
+			test.callback()
+			console.log(`ok ${test.name}`)
+		} catch (error) {
+			console.log(`fail ${error}`)
+		}
+		for (const afterEachCallback of afterEachs) {
+			afterEachCallback()
+		}
+	}
+
+	for (const afterAllCallback of afterAlls) {
+		afterAllCallback()
 	}
 }
